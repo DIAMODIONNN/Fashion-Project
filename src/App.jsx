a3d01e20a27f434e47b6197eace4f981ab114983
Alaa-Mahmoudd <alaa.mahmoud17002@gmail.com>
2024-10-12 02:38:43 +0300

    Signup/Login/Cart/Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import LayoutUser from "./LayoutUser";
 import LayoutAdmin from "./LayoutAdmin";
+import axios from "axios";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users");
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const addUser = async (newUser) => {
+    const isDuplicate = users.some(
+      (user) => user.email === newUser.email || user.name === newUser.name
+    );
+
+    if (isDuplicate) {
+      console.error("Account Already Exists.");
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3000/users", newUser);
+      setUsers((prevUsers) => [...prevUsers, response.data]);
+    } catch (error) {
+      console.error("Error adding user:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const addToCart = (product) => {
+    setCartItems((prevItems) => {
+      const itemExists = prevItems.find((item) => item.id === product.id);
+
+      if (itemExists) {
+        return prevItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [
+          ...prevItems,
+          {
+            id: product.id,
+            name: product.title,
+            price: product.price,
+            img: product.image,
+            quantity: 1,
+          },
+        ];
+      }
+    });
+  };
+
   return (
     <div className="text-center">
       <Routes>
-        <Route path="/*" element={<LayoutUser />} />
-        <Route path="/admin/*"element={<LayoutAdmin/>}/>
+        <Route
+          path="/*"
+          element={
+            <LayoutUser
+              products={products}
+              setProducts={setProducts}
+              addUser={addUser}
+              users={users}
+              addToCart={addToCart}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+            />
+          }
+        />
+        <Route path="/admin/*" element={<LayoutAdmin />} />
       </Routes>
     </div>
   );
--- src/LayoutUser.jsx
+++ src/LayoutUser.jsx
@@ -1,26 +1,58 @@
-import React from 'react'
-import Header from './components/Header'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/user/Home' 
-import Login from './pages/user/Login'
-import Signup from './pages/user/Signup'
-import Shop from './pages/user/Shop'
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/user/Home";
+import Login from "./pages/user/Login";
+import Signup from "./pages/user/Signup";
+import Shop from "./pages/user/Shop";
+import Cart from "./pages/user/Cart";
 
-
-const LayoutUser = () => {
+const LayoutUser = ({
+  products,
+  setProducts,
+  addUser,
+  users,
+  addToCart,
+  cartItems,
+  setCartItems,
+}) => {
   return (
-    <div>          
-        <Header />
-        
-        <Routes>
-            <Route path = '/' element = {<Home />}/>
-            <Route path = '/shop' element = {<Shop />}/>
-            <Route path = '/login' element = {<Login />}/>
-            <Route path = '/signup' element = {<Signup />}/>
-            <Route path = '/*' element = {<Home />}/>
-        </Routes>
-    </div>
-  )
-}
+    <div>
+      <Header />
 
-export default LayoutUser
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/shop"
+          element={
+            <Shop
+              products={products}
+              setProducts={setProducts}
+              addToCart={addToCart}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+            />
+          }
+        />
+        <Route path="/login" element={<Login users={users} />} />
+        <Route
+          path="/signup"
+          element={<Signup addUser={addUser} users={users} />}
+        />
+        <Route
+          path="/cart"
+          element={
+            <Cart
+              addToCart={addToCart}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+            />
+          }
+        />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default LayoutUser;
