556b6c07fc9b2728b25d4aba5720ddc8dac3640c
Amr <amrmohamed23033@gmail.com>
2024-10-14 04:21:35 +0300

    Validation AddUsers

--- src/pages/admin/user/AddUsers.jsx
+++ src/pages/admin/user/AddUsers.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, CardBody } from "@material-tailwind/react";
+import { Button, Card, CardBody, Input , Select , Option, Typography } from "@material-tailwind/react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
@@ -9,16 +9,40 @@ const AddNewUser = ({ setDeleted, deleted }) => {
     name: '',
     email: '',
     password: '',
-    role: 'user', // Set default value
-    gender: 'Male', // Set default value
-    img: '', // Correct field name
+    role: '', 
+    gender: '', 
+    img: '', 
   });
 
+  
+
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+
+  const validateInputs = () => {
+  
+    if (!user.name || user.name.length < 3) {
+      return {username : "Username must be at least 3 characters long."};
+    }else if (!user.email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
+     return{ email : "Please enter a valid email address."};
+    }else if (!user.password || user.password.length < 5) {
+      return {password : "Password should be at least 5 characters long."};
+    }else if (!user.img ) {
+      return {img : "Please provide a valid image URL."};
+    }else if (!user.gender) {
+      return {gender : "Please choose a Gender before proceeding."};
+    }else if (!user.role) {
+      return {role : "Please choose a Role before proceeding."};
+    }
+  
+    return {};
+  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    const validationErrors = validateInputs();
+    if(Object.keys(validationErrors).length===0){
     axios({
       method: "post",
       url: "http://localhost:3000/users",
@@ -33,111 +57,123 @@ const AddNewUser = ({ setDeleted, deleted }) => {
         timer: 2000,
       }).then(() => navigate("/admin/users"));
     });
-  };
+  }else {
+    setErrors(validationErrors);
+  }
+}
+return (
+  <div className="flex justify-center items-center">
+    <Card color="transparent" shadow={false} className="p-6 bg-white max-w-lg w-full rounded-lg shadow-lg mt-6">
+      <Typography variant="h4" color="blue-gray" className="text-center mb-4">
+      Add New User
+      </Typography>
+      <Typography color="gray" className="text-center mb-8">
+        Fill in the details of the new user.
+      </Typography>
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className="space-y-4">
+          
+          <div>
+            <Input
+              label="Username"
+              className="bg-white"
+              value={user.name}
+              error = {errors.username}
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+            />
+            {errors.username && <p className="text-red-500 text-sm mt-1">{errors.username}</p>}
+          </div>
 
-  return (
-    <div className="w-full bg-gradient-to-r from-blue-200 to-indigo-500 py-10">
-      <div className="flex flex-col items-center bg-white max-w-7xl mx-auto p-8 shadow-lg rounded-lg">
-        <h1 className="text-5xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-indigo-500 mb-6">
-          Add New User
-        </h1>
+          
+          <div>
+            <Input
+              label="Email"
+              className="bg-white"
+              value={user.email}
+              error={errors.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            {errors.email && !errors.username && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
+          </div>
 
-        <Card className="w-full bg-[#bae6fd] shadow-md rounded-lg">
-          <CardBody>
-            <form onSubmit={handleSubmit} className="space-y-6">
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="name">
-                  Name:
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  value={user.name}
-                  onChange={(e) => setUser({ ...user, name: e.target.value })}
-                  required
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="email">
-                  Email:
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  value={user.email}
-                  onChange={(e) => setUser({ ...user, email: e.target.value })}
-                  required
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="password">
-                  Password:
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  value={user.password}
-                  onChange={(e) => setUser({ ...user, password: e.target.value })}
-                  required
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-              </div>
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="role">
-                  Role:
-                </label>
-                <select
-                  id="role"
-                  value={user.role}
-                  onChange={(e) => setUser({ ...user, role: e.target.value })}
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                >
-                  <option value="user">User</option>
-                  <option value="admin">Admin</option>
-                </select>
-              </div>
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="gender">
-                  Gender:
-                </label>
-                <select
-                  id="gender"
-                  value={user.gender}
-                  onChange={(e) => setUser({ ...user, gender: e.target.value })}
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                >
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
-              <div>
-                <label className="block text-gray-700 font-semibold mb-2" htmlFor="image">
-                  Image URL:
-                </label>
-                <input
-                  type="url"
-                  id="img" // Changed id to "image"
-                  value={user.img} // Use correct field name
-                  onChange={(e) => setUser({ ...user, img: e.target.value })}
-                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                />
-              </div>
-              <Button
-                type="submit"
-                variant="gradient"
-                color="blue"
-                className="w-full mt-4 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+          
+          <div>
+            <Input
+              label="Password"
+              type="password"
+              className="bg-white"
+              value={user.password}
+              error={errors.password }
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+            {errors.password && !errors.email && !errors.username && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
+          </div>
+
+          <div>
+            <Input
+              label="Image"
+              className="bg-white"
+              type="url"
+              value={user.img}
+              error={errors.img}
+              onChange={(e) => setUser({ ...user, img: e.target.value })}
+            />
+            {errors.img  && !errors.password && !errors.email && !errors.username &&(
+                <p className="text-red-500 text-sm mt-1">{errors.img}</p>
+              )}
+          </div> 
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div>
+          <Select
+                label = "Gender"
+                value={user.gender}
+                error = {errors.gender}
+                onChange={(value) => setUser({ ...user, gender: value })}
+                className= "bg-white"
               >
-                Add User
-              </Button>
-            </form>
-          </CardBody>
-        </Card>
+                <Option value="Male">Male</Option>
+                <Option value="Female">Female</Option>
+              </Select>
+              {errors.gender && !errors.img && !errors.password && !errors.email && !errors.username &&(
+                <p className="text-red-500 text-sm mt-1">{errors.gender}</p>
+              )}
+          </div>
+
+          <div>
+              <Select
+                label = "Role"
+                value={user.role}
+                error = {errors.role}
+                onChange={(value) => setUser({ ...user, role: value })}
+                className= "bg-white"
+              >
+                <Option value="user">User</Option>
+                <Option value="admin">Admin</Option>
+              </Select>
+              {errors.role && !errors.gender && !errors.img && !errors.password && !errors.email && !errors.username &&(
+                <p className="text-red-500 text-sm mt-1">{errors.role}</p>
+              )}
+              </div>
+              </div>
+        </div>
+
+        <div className="flex justify-center items-center gap-4">
+          <Button className="w-full" type="submit">
+            Confirm
+          </Button>
+          <Button color="cyan" onClick={() => navigate(-1)}>
+            Back
+          </Button>
+        </div>
+      </form>
+      </Card>
       </div>
-    </div>
-  );
+);
 };
 
 export default AddNewUser;
