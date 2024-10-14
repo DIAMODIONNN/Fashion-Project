4915b8db04099850a474c2bbdf0c72b100f9b3b5
godzila <saifgodzila@gmail.com>
2024-10-14 20:25:55 +0300

    hadle merge

--- src/LayoutAdmin.jsx
+++ src/LayoutAdmin.jsx
@@ -64,7 +64,7 @@ const LayoutAdmin = ({
           }
         />
 
-<<<<<<< HEAD
+
         {/* Routes for Product >> pages*/}
         <Route
           path="/products"
@@ -121,10 +121,9 @@ const LayoutAdmin = ({
 
         {/* Route for NotFound page*/}
         <Route path="/NotFound" element={<NotFound />} />
-=======
-          {/* Route for NotFound page*/}
-          <Route path="/*" element={<NotFound />} />
->>>>>>> c3b02c9a5b9e248be3180252eef9be366ebcdde6
+
+
+
       </Routes>
     </div>
   );
